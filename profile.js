189360314b6f005a48dbe9a96fa500b9457d9c6a
Sue Rasmussen <cardnoles10@gmail.com>
2015-12-06 12:44:46 -0500

    basic beginning

---
+++ profile.js
@@ -0,0 +1,24 @@
+All code is commented out on purpose
+
+/*var $ = function(id) {
+  alert("$ function is working");
+  return document.getElementById(id);
+}
+alert("this works");
+
+var clickedMe = function() {
+  alert("you have clicked the CLICK ME button");
+}
+
+var clickedToo = function() {
+    alert("you have clicked the CLICK ME TOO button");
+
+}
+
+window.onload = function () {
+  $("me").onclick = clickedMe;
+  $("too").onclick  =clickedToo;
+}*/
+
+
+
