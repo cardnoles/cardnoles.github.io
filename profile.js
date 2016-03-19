b4195dd15e6d0beadda147199ee32d634fb2cb36
Sue Rasmussen <cardnoles10@gmail.com>
2016-03-19 08:53:49 -0400

    minor changes

--- profile.js
+++ profile.js
@@ -3,7 +3,7 @@
 $(document).ready(function(){
   console.log("this is connected");
 
-function fromRight() {
+  function fromRight() {
   $('.slider2').stop().animate({
       right: 0
     }, 400);
@@ -15,11 +15,26 @@ function fromRight() {
     }, 400);
     };
 
+  function fromRight2() {
+    $('.slider4').stop().animate({
+      right: 0
+    }, 400);
+  };
 
+  function fromLeft2() {
+    $('.slider3').stop().animate({
+      left: 0
+    }, 400);
+    };
 
     $('#home').on("click", function(){
       fromRight();
       fromLeft();
+    });
+
+    $('#aboutMe').on("click", function(){
+      fromRight2();
+      fromLeft2();
     });
 
 });
