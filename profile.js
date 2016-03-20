1ef28afc742fa50a510dfc3b8fe140282e9ee009
Sue Rasmussen <cardnoles10@gmail.com>
2016-03-19 21:23:17 -0400

    add interests

--- profile.js
+++ profile.js
@@ -3,20 +3,51 @@
 $(document).ready(function(){
   console.log("this is connected");
 
-  function fromRight() {
-  $('.slider2').stop().animate({
-      right: 0
+  function clearHome() {
+    $('.slider1').stop().animate({
+      left: -1850
+    }, 800);
+    $('.slider2').stop().animate({
+      right: -1850
+    }, 800);
+  };
+
+  function clearAboutMe() {
+    $('.slider3').stop().animate({
+      left: -650
+    }, 400);
+    $('.slider4').stop().animate({
+      right: -650
     }, 400);
   };
+
+  function clearEducation() {
+    $('.slider5').stop().animate({
+      left: -1050
+    }, 400);
+    $('.slider6').stop().animate({
+      right: -1050
+    }, 400);
+  };
+
+  function clearInterests() {
+    $('.slider7').stop().animate({
+      left: -1450
+    }, 400);
+    $('.slider8').stop().animate({
+      right: -1450
+    }, 400);
+  };
+
 
   function fromLeft() {
     $('.slider1').stop().animate({
       left: 0
     }, 400);
-    };
+  };
 
-  function fromRight2() {
-    $('.slider4').stop().animate({
+  function fromRight() {
+    $('.slider2').stop().animate({
       right: 0
     }, 400);
   };
@@ -25,16 +56,69 @@ $(document).ready(function(){
     $('.slider3').stop().animate({
       left: 0
     }, 400);
-    };
+  };
+
+  function fromRight2() {
+    $('.slider4').stop().animate({
+      right: 0
+    }, 400);
+  };
+
+  function fromLeft3() {
+    $('.slider5').stop().animate({
+      left: 0
+    }, 400);
+  };
+
+  function fromRight3() {
+    $('.slider6').stop().animate({
+      right: 0
+    }, 400);
+  };
+
+  function fromLeft4() {
+    $('.slider7').stop().animate({
+      left: 0
+    }, 400);
+  };
+
+  function fromRight4() {
+    $('.slider8').stop().animate({
+      right: 0
+    }, 400);
+  };
+
 
     $('#home').on("click", function(){
       fromRight();
       fromLeft();
+      clearAboutMe();
+      clearEducation();
+      clearInterests();
     });
 
     $('#aboutMe').on("click", function(){
       fromRight2();
       fromLeft2();
+      clearHome();
+      clearEducation();
+      clearInterests();
+    });
+
+    $('#education').on("click", function(){
+      fromRight3();
+      fromLeft3();
+      clearHome();
+      clearAboutMe();
+      clearInterests();
+    });
+
+     $('#interests').on("click", function(){
+      fromRight4();
+      fromLeft4();
+      clearHome();
+      clearAboutMe();
+      clearEducation();
     });
 
 });
